8c6263520166b5cbd3bcb35707d6fbb66e3bc010
Trilap <117962811+Trilap@users.noreply.github.com>
2022-11-11 11:06:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Hello bà zà",
-    introTitle: 'Bé Chan',
+    titleWeb: "Hello bà zà,Wep này dành riêng cho Diễm Trang    ",
+    introTitle: 'Em Yêu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: 'Trí Lập Trẻ Trâu chưa :))))',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu mình anh <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu, cỡ nào cũng phải yêu. vì Trí Lập cute,yêu bé Chan nhất <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 8h, mình ra công viên chơi .',
+    messDesc: 'Hong biết tối nay hay mai có hẹn được Bé Chan đi chơi ko  .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100083922157757' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'http://output.jsbin.com/denanuq' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
